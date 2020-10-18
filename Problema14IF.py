1760f9c8f0b27de4c0625ab245cb41b7c3ea90dc
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-10-18 18:54:47 +0300

    Update Problema14IF.py

--- Problema14IF.py
+++ Problema14IF.py
@@ -4,10 +4,5 @@ Ionel  a  sosit  al  n-lea. În a câta căsuţă se va afla?
 Exemplu: date de intrare: n=69 date de ieşire: casuta 17.
 """
 
-a=int(input("Al catelea a venit Ionel:"))
-b=0
-c=0
-while c<a:
-    c=c+4
-    b=b+1
-print(b)
+n = int(input("Ionel a venit al: "))
+print("Se pune in a", (n - 1) // 4 + 1, "casuta")
